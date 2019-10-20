5fc6017d27d26fa1450e188410a8f2f8a9ceeb50
shanmulinxi <shanmulinxi@163.com>
2019-10-20 23:04:57 +0800

    test

--- express/web/public/page/static/static.js
+++ express/web/public/page/static/static.js
@@ -342,3 +342,13 @@ var customLoading = text => {
 var customLoadingClose = () => {
   g_Loading.close()
 }
+
+// 注册一个全局自定义指令 `v-focus`
+Vue.directive('autofocus', {
+  // 当被绑定的元素插入到 DOM 中时……
+  inserted: function (el) {
+    // 聚焦元素
+    el.focus()
+
+  }
+})
